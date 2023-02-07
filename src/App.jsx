1e9c7d41a4fafdd1c1111b5ebee84cbf772539e7
peppeconti <46853326+peppeconti@users.noreply.github.com>
2023-02-07 12:29:16 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
-import React from 'react';
 import Home from './components/Home/Home';
 import Query from './components/Query/Query';
 import { Route, Routes } from 'react-router-dom';
-import './App.css';
 // IMPORT FONTAWESOME
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faSearch, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
-library.add(faSearch, faTimes);
+library.add(faSearch);
 
 function App() {
 
   return (
-    <div className='App'>
+    <>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/search' element={<Query />} />
       </Routes>
-    </div>
+    </>
   );
 }
 
