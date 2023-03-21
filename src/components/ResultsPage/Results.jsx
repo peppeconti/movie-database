d35bf30c3d9347844562303116eaf777055a0dd1
peppeconti <46853326+peppeconti@users.noreply.github.com>
2023-03-21 13:16:27 +0100

    update

--- src/components/ResultsPage/Results.jsx
+++ src/components/ResultsPage/Results.jsx
@@ -1,12 +1,45 @@
 import classes from './Results.module.css';
+import useFetch from "../../hooks/useFetch";
+import useQuery from "../../hooks/useQuery";
 import Pagination from './Pagination';
 
-const Results = ({data, page}) => {
+const Results = ({ page, setPage }) => {
+
+    const { queryString } = useQuery();
+
+    const queryStr = queryString();
+    const apikey = process.env.REACT_APP_API_KEY;
+    const API = `http://www.omdbapi.com/?apikey=${apikey}&${queryStr}&page=${page}`;
+
+    const { data, error, loading } = useFetch(API);
+
+    let movies;
+    let totalMovies;
+    let content;
+
+    if (data && data.Response === 'True') {
+        movies = data.Search.map(e => {
+            return { id: e.imdbID, title: e.Title, img: e.Poster }
+        });
+        totalMovies = data.totalResults;
+        content = <>
+            <div className={classes.result}>
+                {movies.map(movie => <figure key={movie.id}><img src={movie.img} alt={movie.title} /></figure>)}
+            </div>
+            <Pagination totalCount={totalMovies} currentPage={page} setPage={setPage} />
+        </>
+    }
+
+    if (data && data.Response === 'False') {
+        content = <div>No results...sorry</div>
+    }
+
+    console.log(data);
 
     return (
         <div className={classes.results}>
-            <p className={classes.total}></p>
-          
+            <p className={classes.total}>You found {totalMovies} items</p>
+            {content}
         </div>
     )
 }
--- src/components/ResultsPage/ResultsPage.jsx
+++ src/components/ResultsPage/ResultsPage.jsx
@@ -1,23 +1,12 @@
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 import classes from './ResultsPage.module.css';
 import Search from "../Shared/Search";
 import Results from './Results';
-import useFetch from "../../hooks/useFetch";
-import useQuery from "../../hooks/useQuery";
 
 const ResultsPage = () => {
 
     // HOOKS
     const [page, setPage] = useState(1);
-    const { queryString } = useQuery();
-
-    const queryStr = queryString();
-    const apikey = process.env.REACT_APP_API_KEY;
-    const API = `http://www.omdbapi.com/?apikey=${apikey}&${queryStr}&page=${page}`;
-
-    const { data, error, loading } = useFetch(API);
-
-    console.log(data);
 
     return (
         <section className={classes.results__page}>
@@ -25,8 +14,7 @@ const ResultsPage = () => {
                 <Search setPage={setPage} />
             </div>
             <div className={classes.results__wrapper}>
-                <p className={classes.total}></p>
-                <Results data={data} page={page}/>
+                <Results setPage={setPage} page={page}/>
             </div>
         </section>
     )
