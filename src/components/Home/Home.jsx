c5b1f72c9f0fcdc985863bffa6ad504fc011ed86
peppeconti <46853326+peppeconti@users.noreply.github.com>
2023-04-02 20:13:25 +0200

    update

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,14 +1,13 @@
 import React from "react";
-import classes from './Home.module.css';
+import classes from "./Home.module.css";
 import Search from "../Shared/Search";
 
 const Home = () => {
-
-    return (
-        <section className={classes.home}>
-            <Search />
-        </section>
-    )
-}
+  return (
+    <section className={classes.home}>
+      <Search />
+    </section>
+  );
+};
 
 export default Home;
