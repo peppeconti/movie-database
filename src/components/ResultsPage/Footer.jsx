5950eb1b394b0a4379657b935e55357848149fb3
peppeconti <46853326+peppeconti@users.noreply.github.com>
2023-04-09 12:02:54 +0200

    update

---
+++ src/components/ResultsPage/Footer.jsx
@@ -0,0 +1,7 @@
+import classes from "./Footer.module.css";
+
+const Footer = () => {
+  return <footer className={classes.footer} />;
+};
+
+export default Footer;
--- src/components/ResultsPage/ResultsPage.jsx
+++ src/components/ResultsPage/ResultsPage.jsx
@@ -1,23 +1,24 @@
-import { useState } from 'react';
-import classes from './ResultsPage.module.css';
+import { useState } from "react";
+import classes from "./ResultsPage.module.css";
 import Search from "../Shared/Search";
-import Results from './Results';
+import Results from "./Results";
+import Footer from "./Footer";
 
 const ResultsPage = () => {
+  // HOOKS
+  const [page, setPage] = useState(1);
 
-    // HOOKS
-    const [page, setPage] = useState(1);
-
-    return (
-        <section className={classes.results__page}>
-            <div className={classes.search__wrapper}>
-                <Search setPage={setPage} />
-            </div>
-            <div className={classes.results__wrapper}>
-                <Results setPage={setPage} page={page}/>
-            </div>
-        </section>
-    )
-}
+  return (
+    <section className={classes.results__page}>
+      <div className={classes.search__wrapper}>
+        <Search setPage={setPage} />
+      </div>
+      <div className={classes.results__wrapper}>
+        <Results setPage={setPage} page={page} />
+      </div>
+      <Footer />
+    </section>
+  );
+};
 
 export default ResultsPage;
