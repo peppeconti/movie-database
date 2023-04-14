479e986a9a5854217b3247dbd0b51997a9cf5ab8
peppeconti <46853326+peppeconti@users.noreply.github.com>
2023-04-14 09:40:38 +0200

    update

--- src/components/ResultsPage/Results.jsx
+++ src/components/ResultsPage/Results.jsx
@@ -1,78 +1,94 @@
-import { useState } from 'react';
-import classes from './Results.module.css';
+import { useState } from "react";
+import classes from "./Results.module.css";
 import useFetch from "../../hooks/useFetch";
 import useQuery from "../../hooks/useQuery";
-import Pagination from './Pagination';
-import Modal from './Modal';
-import SingleItem from './SingleItem';
+import Pagination from "./Pagination";
+import Modal from "./Modal";
+import Spinner from "../Shared/Spinner";
+import SingleItem from "./SingleItem";
 
 const Results = ({ page, setPage }) => {
+  const [modal, setModal] = useState(false);
+  const [modalData, setModalData] = useState(null);
 
-    const [modal, setModal] = useState(false);
-    const [modalData, setModalData] = useState(null);
+  const { queryString } = useQuery();
 
-    const { queryString } = useQuery();
+  const queryStr = queryString();
+  const apikey = process.env.REACT_APP_API_KEY;
+  const API = `https://www.omdbapi.com/?apikey=${apikey}&${queryStr}&page=${page}`;
 
-    const queryStr = queryString();
-    const apikey = process.env.REACT_APP_API_KEY;
-    const API = `https://www.omdbapi.com/?apikey=${apikey}&${queryStr}&page=${page}`;
+  const { data, error, loading } = useFetch(API);
 
-    const { data, error, loading } = useFetch(API);
+  //console.log(data)
 
-    //console.log(data)
+  // DATA
+  let movies;
+  let totalMovies;
 
-    // DATA
-    let movies;
-    let totalMovies;
+  // ELEMENTS
+  let totalFound;
+  let movieList;
+  let pagination;
 
-    // ELEMENTS
-    let totalFound;
-    let movieList;
-    let pagination;
+  if (data && data.Response === "True") {
+    // SET DATA RESULTS
+    movies = data.Search.map((e) => {
+      return {
+        id: e.imdbID,
+        title: e.Title,
+        img: e.Poster,
+        year: e.Year,
+        type: e.Type,
+      };
+    });
+    totalMovies = data.totalResults;
+    // SET ELEMENTS
+    totalFound = <p className={classes.total}>You found {totalMovies} items</p>;
+    movieList = (
+      <ul className={classes.movie_list}>
+        {movies.map((movie, index) => (
+          <SingleItem
+            key={movie.id}
+            id={movie.id}
+            title={movie.title}
+            img={movie.img}
+            year={movie.year}
+            type={movie.type}
+            setModal={setModal}
+            setModalData={setModalData}
+            number={index + 10 * page - 9}
+          />
+        ))}
+      </ul>
+    );
+    pagination = (
+      <Pagination
+        totalCount={totalMovies}
+        currentPage={page}
+        setPage={setPage}
+      />
+    );
+  }
 
-    if (data && data.Response === 'True') {
-        // SET DATA RESULTS
-        movies = data.Search.map(e => {
-            return { id: e.imdbID, title: e.Title, img: e.Poster, year: e.Year, type: e.Type }
-        });
-        totalMovies = data.totalResults;
-        // SET ELEMENTS
-        totalFound = <p className={classes.total}>You found {totalMovies} items</p>
-        movieList = <ul className={classes.movie_list}>
-            {movies.map((movie, index) =>
-                <SingleItem
-                    key={movie.id}
-                    id={movie.id}
-                    title={movie.title}
-                    img={movie.img}
-                    year={movie.year}
-                    type={movie.type}
-                    setModal={setModal}
-                    setModalData={setModalData}
-                    number={index + (10 * page) - 9}
-                />
-            )}
-        </ul>
-        pagination = <Pagination totalCount={totalMovies} currentPage={page} setPage={setPage} />
-    }
+  if (data && data.Response === "False") {
+    totalFound = <p className={classes.total}>You found no items</p>;
+  }
 
-    if (data && data.Response === 'False') {
-        totalFound = <p className={classes.total}>You found no items</p>
-    }
+  //console.log(data);
 
-    //console.log(data);
-
-    return (
-        <div className={classes.results}>
-            {!loading && <>
-                {totalFound}
-                {movieList}
-                {pagination}
-            </>}
-            {loading && <p>loading...</p>}
-            {modal && <Modal setModal={setModal} modalData={modalData} />}
-        </div>
-    )
-}
+  return (
+    <div className={classes.results}>
+      {!loading && (
+        <>
+          {totalFound}
+          {movieList}
+          {pagination}
+        </>
+      )}
+      {loading && <Modal />}
+      {modal && <Modal setModal={setModal} modalData={modalData} />}
+    </div>
+  );
+};
 
 export default Results;
