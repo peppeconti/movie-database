f458bedf1a7b3393879c4ae4ed29ea64c87d1c8c
peppeconti <46853326+peppeconti@users.noreply.github.com>
2023-09-24 22:11:21 +0200

    update

--- src/components/ResultsPage/Footer.jsx
+++ src/components/ResultsPage/Footer.jsx
@@ -1,7 +1,9 @@
 import classes from "./Footer.module.css";
 
 const Footer = () => {
-  return <footer className={classes.footer} />;
+  return <footer className={classes.footer}>
+    <p>Made by Giuseppe Conti using React and the OMDb API</p>
+  </footer>;
 };
 
 export default Footer;
