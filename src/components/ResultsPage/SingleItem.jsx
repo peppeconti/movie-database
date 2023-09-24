0986635d92fbcfbde4c433a7f270318531cbedc0
peppeconti <46853326+peppeconti@users.noreply.github.com>
2023-09-24 20:12:03 +0200

    update

--- src/components/ResultsPage/SingleItem.jsx
+++ src/components/ResultsPage/SingleItem.jsx
@@ -17,11 +17,11 @@ const SingleItem = ({ id, title, img, number, year, type, setModal, setModalData
             </div>
             <figure>
                 <img src={img !== 'N/A' ? img : not_found} alt={title} />
+                <button className={classes.plot} onClick={openModal}>PLOT</button>
             </figure>
             <div className={classes.title}>
-                <p>{title} ({type})</p>
-                <p>{year}</p>
-                <button onClick={openModal}>VIEW</button>
+                <p className={classes.title}>{title} ({type})</p>
+                <p className={classes.year}>{year}</p>
             </div>
         </li>
     )
